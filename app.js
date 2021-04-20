c3edffe653d70415f986512cc7a05c1a83f855a2
매그니토 <anyman@afreecatv.com>
2021-04-20 17:35:37 +0900

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+import { WaveGroup } from "./src/WaveGroup.js";
+
+class App {
+    constructor() {
+        this.canvas = document.createElement("canvas");
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext("2d");
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        this.waveGroup = new WaveGroup();
+
+        window.addEventListener("resize", this.resize.bind(this));
+        window.requestAnimationFrame(this.animate.bind(this));
+        this.resize();
+    }
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+        this.waveGroup.resize(this.stageWidth, this.stageHeight);
+    }
+    animate() {
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+        this.waveGroup.draw(this.ctx);
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+}
+window.onload = () => {
+    new App();
+};
